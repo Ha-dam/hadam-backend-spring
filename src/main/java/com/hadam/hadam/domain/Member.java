dc9b9e679e44874aa84a62b5adfd5596da440535
ziiyouth <ziy913@naver.com>
2023-12-04 02:18:28 +0900

    feat: create member API

--- src/main/java/com/hadam/hadam/domain/Member.java
+++ src/main/java/com/hadam/hadam/domain/Member.java
@@ -1,8 +1,10 @@
 package com.hadam.hadam.domain;
 
+import com.hadam.hadam.dto.request.CreateMemberReq;
 import com.hadam.hadam.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -29,4 +31,11 @@ public class Member extends BaseTimeEntity {
     @Column(length = 50, nullable = false)
     private MemberStatus memberStatus;
 
+    @Builder
+    public Member(CreateMemberReq createMemberReq){
+        this.identifier = createMemberReq.identifier();
+        this.email = createMemberReq.email();
+        this.nickName = createMemberReq.nickName();
+        this.memberStatus = MemberStatus.ACTIVE;
+    }
 }
