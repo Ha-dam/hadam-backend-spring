87645d92172f3fa531efb13f474bdc479ca59e39
ziiyouth <ziy913@naver.com>
2023-12-04 14:54:56 +0900

    feat: create diary entity

---
+++ src/main/java/com/hadam/hadam/domain/Diary.java
@@ -0,0 +1,29 @@
+package com.hadam.hadam.domain;
+
+import com.hadam.hadam.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "diary")
+public class Diary extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "diaryInfo_id")
+    private DiaryInfo diaryInfo;
+
+    @Column(columnDefinition = "TEXT")
+    private String img;
+
+    @Column(columnDefinition = "TEXT")
+    private String content;
+
+}
